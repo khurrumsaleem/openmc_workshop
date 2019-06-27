062413c0fac70e88851aba125b16414097513f1d
shimwell <mail@jshimwell.com>
2019-06-27 18:09:49 +0100

    added example CAD geometry and started work on simulation

---
+++ tasks/task_10/example_CAD_simulations.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+"""example_isotope_plot.py: plots few 2D views of a simple tokamak geometry with neutron flux."""
+
+__author__      = "Jonathan Shimwell"
+
+import openmc
+import matplotlib.pyplot as plt
+import os
+
+#MATERIALS#
+
+breeder_material = openmc.Material(1, "breeder_material") #Pb84.2Li15.8 with natural enrichment of Li6
+enrichment_fraction = 0.50
+breeder_material.add_element('Pb', 84.2,'ao')
+breeder_material.add_nuclide('Li6', enrichment_fraction*15.8, 'ao')
+breeder_material.add_nuclide('Li7', (1.0-enrichment_fraction)*15.8, 'ao')
+breeder_material.set_density('atom/b-cm',3.2720171e-2) # around 11 g/cm3
+
+copper = openmc.Material(name='magnet_material')
+copper.set_density('g/cm3', 8.5)
+copper.add_element('Cu', 1.0)
+
+eurofer = openmc.Material(name='eurofer')
+eurofer.set_density('g/cm3', 7.75)
+eurofer.add_element('Fe', 89.067, percent_type='wo')
+eurofer.add_element('C', 0.11, percent_type='wo')
+eurofer.add_element('Mn', 0.4, percent_type='wo')
+eurofer.add_element('Cr', 9.0, percent_type='wo')
+eurofer.add_element('Ta', 0.12, percent_type='wo')
+eurofer.add_element('W', 1.1, percent_type='wo')
+eurofer.add_element('N', 0.003, percent_type='wo')
+eurofer.add_element('V', 0.2, percent_type='wo')
+
+mats = openmc.Materials([breeder_material, eurofer, copper])
+
+
+#GEOMETRY#
+
+universe = openmc.Universe()
+geom = openmc.Geometry(universe)
+
+
+
+
+#SIMULATION SETTINGS#
+
+# Instantiate a Settings object
+sett = openmc.Settings()
+batches = 2
+sett.batches = batches
+sett.inactive = 0
+sett.particles = 50
+sett.run_mode = 'fixed source'
+sett.dagmc = True
+
+# Create a DT point source
+source = openmc.Source()
+source.space = openmc.stats.Point((300,0,0))
+source.angle = openmc.stats.Isotropic()
+source.energy = openmc.stats.Discrete([14e6], [1])
+sett.source = source
+
+
+tallies = openmc.Tallies()
+
+#added a cell tally for tritium production
+cell_filter = openmc.CellFilter(1)
+tbr_tally = openmc.Tally(2,name='TBR')
+tbr_tally.filters = [cell_filter]
+tbr_tally.scores = ['(n,t)'] #or 205
+tallies.append(tbr_tally)
+
+
+# Run OpenMC!
+model = openmc.model.Model(geom, mats, sett, tallies)
+model.run()
+
+# open the results file
+sp = openmc.StatePoint('statepoint.'+str(batches)+'.h5')
+
+# access the tally
+tbr_tally = sp.get_tally(name='TBR')
+df = tbr_tally.get_pandas_dataframe()
+
+tbr_tally_result = df['mean'].sum()
+tbr_tally_std_dev = df['std. dev.'].sum()
+
+
+print('The tritium breeding ratio was found, TBR = ',tbr_tally_result)
+print('error on the tbr tally is ',tbr_tally_std_dev)
